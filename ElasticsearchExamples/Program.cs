30842172bf0b541fd2e1614a17aceef07f40dd88
Steve Gordon <sgordon@hotmail.co.uk>
2021-11-12 13:38:28 +0000

    Final example

--- ElasticsearchExamples/Program.cs
+++ ElasticsearchExamples/Program.cs
@@ -1,24 +1,98 @@
-﻿namespace ElasticsearchExamples;
+﻿using Nest;
+
+namespace ElasticsearchExamples;
 
 internal class Program
 {
     private const string IndexName = "stock-demo-v1";
 
-    // TODO: Create client
+    public static IElasticClient Client = new ElasticClient(new ConnectionSettings().DefaultIndex(IndexName));
 
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        // TODO: Check for existing index
+        var existsResponse = await Client.Indices.ExistsAsync(IndexName);
 
-        // TODO: Create and seed index if it does not exist
+        if (!existsResponse.Exists)
+        {
+            var newIndexResponse = await Client.Indices.CreateAsync(IndexName, i => i
+                .Map(m => m
+                    .AutoMap<StockData>()
+                    .Properties<StockData>(p => p.Keyword(k => k.Name(n => n.Symbol))))
+                .Settings(s => s.NumberOfShards(1).NumberOfReplicas(1)));
 
-        // TODO: Search for term "MSFT"
+            if (!newIndexResponse.IsValid || newIndexResponse.Acknowledged is false) throw new Exception("Oh no!");
 
-        // TODO: Search to query for data where name contains "inc"
+            var bulkAll = Client.BulkAll(ReadStockData(), r => r
+                .Index(IndexName)
+                .BackOffRetries(2)
+                .BackOffTime("30s")
+                .MaxDegreeOfParallelism(4)
+                .Size(1000));
 
-        // TODO: Search and aggregate on trade volumes by month
+            bulkAll.Wait(TimeSpan.FromMinutes(10), r => Console.WriteLine("Data indexed"));
+        }
 
-        // TODO: Scroll all
+        var symbolResponse = await Client.SearchAsync<StockData>(s => s
+            .Index(IndexName)
+            .Query(q => q
+                .Bool(b => b
+                    .Filter(f => f
+                        .Term(t => t.Field(fld => fld.Symbol).Value("MSFT")))))
+            .Size(20)
+            .Sort(srt => srt.Descending(d => d.Date)));
+
+        if (!symbolResponse.IsValid) throw new Exception("Oh no");
+
+        foreach (var data in symbolResponse.Documents)
+        {
+            //Console.WriteLine($"{data.Date}   {data.High} {data.Low}");
+        }
+
+        var fullTextSearchResponse = await Client.SearchAsync<StockData>(s => s.Index(IndexName)
+            .Query(q => q
+                .Match(m => m.Field(f => f.Name).Query("inc")))
+            .Size(20)
+            .Sort(srt => srt.Descending(d => d.Date)));
+
+        foreach (var data in fullTextSearchResponse.Documents)
+        {
+            //Console.WriteLine($"{data.Name} {data.Date}   {data.High} {data.Low}");
+        }
+
+        var aggExampleResponse = await Client.SearchAsync<StockData>(s => s
+            .Index(IndexName)
+            .Size(0)
+            .Query(q => q
+                .Bool(b => b
+                    .Filter(f => f
+                        .Term(t => t.Field(fld => fld.Symbol).Value("MSFT")))))
+            .Aggregations(a => a
+                .DateHistogram("by-month", dh => dh
+                    .CalendarInterval(DateInterval.Month)
+                    .Field(fld => fld.Date)
+                    .Order(HistogramOrder.KeyDescending)
+                    .Aggregations(agg => agg
+                        .Sum("trade-volumes", sum => sum.Field(fld => fld.Volume))))));
+
+        var monthlyBuckets = aggExampleResponse.Aggregations.DateHistogram("by-month").Buckets;
+
+        foreach (var monthlyBucket in monthlyBuckets)
+        {
+            var volume = monthlyBucket.Sum("trade-volumes").Value;
+            //Console.WriteLine($"{monthlyBucket.Date} : {volume:n0}");
+        }
+
+        var scrollAllObservable = Client.ScrollAll<StockData>("10s", Environment.ProcessorCount, scroll => scroll
+            .Search(s => s.Index(IndexName).MatchAll().Size(100))
+            .MaxDegreeOfParallelism(Environment.ProcessorCount));
+
+        scrollAllObservable.Wait(TimeSpan.FromMinutes(5), s =>
+        {
+            foreach (var doc in s.SearchResponse.Documents)
+            {
+                Console.WriteLine(doc.Symbol);
+            }
+        });
     }
 
     public static IEnumerable<StockData> ReadStockData()
