e0c39f8cc31179e01fbc245dc0e483c536b103db
Steve Gordon <sgordon@hotmail.co.uk>
2022-04-25 12:05:14 +0100

    Porting more examples

--- ElasticsearchExamples/Program.cs
+++ ElasticsearchExamples/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Elastic.Clients.Elasticsearch;
 using Elastic.Clients.Elasticsearch.Helpers;
 using Elastic.Clients.Elasticsearch.Mapping;
+using Elastic.Clients.Elasticsearch.QueryDsl;
 using Elastic.Transport;
 using ElasticsearchExamples;
 
@@ -8,7 +9,8 @@ const string IndexName = "stock-demo-v1";
 
 var settings = new ElasticsearchClientSettings(new Uri("https://localhost:9200"))
     .CertificateFingerprint("E8:76:3D:91:81:8C:57:31:6F:2F:E0:4C:17:78:78:FB:38:CC:37:27:41:7A:94:B4:12:AA:B6:D1:D6:C4:4C:7D")
-    .Authentication(new BasicAuthentication("elastic", "password"));
+    .Authentication(new BasicAuthentication("elastic", "password"))
+    .EnableDebugMode();
 
 var client = new ElasticsearchClient(settings);
 
@@ -33,38 +35,69 @@ if (!existsResponse.Exists)
 
     if (!newIndexResponse.IsValid || newIndexResponse.Acknowledged is false) throw new Exception("Oh no!");
 
-    try
-    {
-        var bulkAll = client.BulkAll(ReadStockData(), r => r
-           .Index(IndexName)
-           .BackOffRetries(20)
-           .BackOffTime(TimeSpan.FromSeconds(10))
-           .ContinueAfterDroppedDocuments()
-           .DroppedDocumentCallback((r, d) =>
-           {
-               Console.WriteLine(r.Error.Reason);
-           })
-           .MaxDegreeOfParallelism(4)
-           .Size(1000));
+    var bulkAll = client.BulkAll(ReadStockData(), r => r
+        .Index(IndexName)
+        .BackOffRetries(20)
+        .BackOffTime(TimeSpan.FromSeconds(10))
+        .ContinueAfterDroppedDocuments()
+        .DroppedDocumentCallback((r, d) =>
+        {
+            Console.WriteLine(r.Error.Reason);
+        })
+        .MaxDegreeOfParallelism(4)
+        .Size(1000));
 
-        bulkAll.Wait(TimeSpan.FromMinutes(10), r => Console.WriteLine("Data indexed"));
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.ToString());
-    }
+    bulkAll.Wait(TimeSpan.FromMinutes(10), r => Console.WriteLine("Data indexed"));
 }
+
+var symbolResponse = await client.SearchAsync<StockData>(s => s
+        .Index(IndexName)
+        .Query(q => q
+            .Bool(b => b
+                .Filter(new[] { new QueryContainer(new TermQuery { Field = "symbol", Value = "MSFT" }) })))
+        .Size(20)
+        .Sort(srt => srt.Descending(d => d.Date)));
+
+foreach (var data in symbolResponse.Documents)
+{
+    //Console.WriteLine($"{data.Date}   {data.High:n2} {data.Low:n2}");
+}
+
+//var symbolResponse = await client.SearchAsync<StockData>(s => s
+//        .Index(IndexName)
+//        .Query(q => q
+//            .Bool(b => b
+//                .Filter(f => f
+//                    .Term(t => t.Field(fld => fld.Symbol).Value("MSFT")))))
+//        .Size(20)
+//        .Sort(srt => srt.Descending(d => d.Date)));
+
+var fullTextSearchResponse = await client.SearchAsync<StockData>(s => s.Index(IndexName)
+    .Query(q => q
+        .Match(m => m.Field(f => f.Name).Query("inc")))
+    .Size(20)
+    .Sort(srt => srt.Descending(d => d.Date)));
+
+foreach (var data in fullTextSearchResponse.Documents)
+{
+    Console.WriteLine($"{data.Name} {data.Date}   {data.High:n2} {data.Low:n2}");
+}
+
+if (!symbolResponse.IsValid) throw new Exception("Oh no");
+
+
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
 
 static IEnumerable<StockData> ReadStockData()
 {
     var file = new StreamReader("c:\\stock-data\\all_stocks_5yr.csv");
+    file.ReadLine(); // Skip the header
 
     string line;
     while ((line = file.ReadLine()) is not null)
     {
-        yield return new StockData(line);
+        yield return StockData.ParseFromFileLine(line);
     }
 }
-
-Console.WriteLine("Press any key to exit.");
-Console.ReadKey();
--- ElasticsearchExamples/StockData.cs
+++ ElasticsearchExamples/StockData.cs
@@ -1,4 +1,6 @@
-﻿namespace ElasticsearchExamples;
+﻿using System.Globalization;
+
+namespace ElasticsearchExamples;
 
 public class StockData
 {
@@ -10,40 +12,41 @@ public class StockData
         { "M", "Macy's inc" }
     };
 
-    public StockData(string dataLine)
-    {
-        var columns = dataLine.Split(',', StringSplitOptions.TrimEntries);
-
-        if (DateTime.TryParse(columns[0], out var date))
-            Date = date;
-
-        if (double.TryParse(columns[1], out var open))
-            Open = open;
-
-        if (double.TryParse(columns[2], out var high))
-            High = high;
-
-        if (double.TryParse(columns[3], out var low))
-            Low = low;
-
-        if (double.TryParse(columns[4], out var close))
-            Close = close;
-
-        if (uint.TryParse(columns[5], out var volume))
-            Volume = volume;
-
-        Symbol = columns[6];
-
-        if (CompanyLookup.TryGetValue(Symbol, out var name))
-            Name = name;
-    }
-
     public DateTime Date { get; init; }
     public double Open { get; init; }
     public double Close { get; init; }
     public double High { get; init; }
     public double Low { get; init; }
-    public uint Volume { get; init; }
+    public int Volume { get; init; }
     public string Symbol { get; init; }
     public string Name { get; init; }
+
+    public static StockData ParseFromFileLine(string dataLine)
+    {
+        var columns = dataLine.Split(',', StringSplitOptions.TrimEntries);
+
+        var date = DateTime.Parse(columns[0]);
+
+        _ = float.TryParse(columns[1], out float open);
+        _ = float.TryParse(columns[1], out float high);
+        _ = float.TryParse(columns[1], out float low);
+        _ = float.TryParse(columns[1], out float close);
+
+        var volume = int.Parse(columns[5]);
+        var symbol = columns[6];
+
+        CompanyLookup.TryGetValue(symbol, out string name);
+
+        return new StockData
+        {
+            Name = name,
+            Date = date,
+            Open = open,
+            Close = close,
+            High = high,
+            Low = low,
+            Volume = volume,
+            Symbol = symbol
+        };
+    }
 }
